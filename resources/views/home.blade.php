faf5a90ad01f50d0ecbd6b320ea8e846779c46b6
Lopeaa <lopeaa@gmail.com>
2017-06-05 19:29:31 +0200

    First project commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <blockquote>There are two theories to arguing with a woman. Neither works.</blockquote>
+    
+      <h2>Pills &amp Resources</h2>
+  	<p></p>
+  		<ul class="nav nav-pills">
+    			<li class='active'><a data-toggle="pill" href="#certauth">Cetificate Authority</a></li>
+    			<li><a data-toggle="pill" href="#bcapi">BlueCoat API Client </a></li>
+    			<li><a data-toggle="pill" href="#2falatch">2FA + Latch</a></li>
+    			<li><a data-toggle="pill" href="#py">Python Scripts</a></li>
+          <li><a data-toggle="pill" href="#py">Github</a></li>
+    			<li><a data-toggle="pill" href="#le">Let's Encrypt Web App</a></li>
+  		</ul>
+    	<div class="tab-content">
+
+    	<!-- ################################################################## -->
+    	<div id="certauth" class="tab-pane fade in active">  
+      	<h3>Certificate Authority</h3>
+      	<p>Management WebApp for Certificate Authority.</p>
+      		<p>URL: <a href="https://ca.liquabit.com"><span class=""></span>ca.liquabit.com</a></p>
+          <p>Latest archive: <a href=""><span class=""></span>Prototypes-2017531-19:47:50.tgz</a></p>
+
+			<p><strong>Log in with 'Username' instead of 'email' in Laravel Framework.</strong></p>
+				<p>In order to log in with 'username' instead of 'email' field, you have to include the field
+				 'username' in the database fist like this:
+				 Edit database/migrations/*_create_users_table.php and include these fields:
+				 <code>$table->string('username', 60);</code>
+Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+	    	$table->string('username', 10);
+	    	$table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+			</pre>
+			Then, execute <code>php artisan migrate</code> to create de tables in the DB and <code>php artisan make:auth</code> to create the Login/Register pages.</p>
+			<p>Now, you have to modify the pages to include the 'Username' field.</p>
+			
+    	</div>
+    	<!-- ################################################################## -->
+    	<div id="bcapi" class="tab-pane fade">
+      	<h3>BlueCoat API Client</h3>
+      	<p></p>
+    	</div>
+		<div id="2falatch" class="tab-pane fade">
+      	<h3>Laravel Framework + 2FA + Latch</h3>
+      	<p></p>
+    	</div>
+    	<!-- ################################################################### -->
+    	<div id="py" class="tab-pane fade">
+      	<h3>Python Scripts</h3>
+      	<p></p>
+    	</div>
+    	
+    	<!-- #################################################################### -->
+       <div id="py" class="tab-pane fade">
+        <h3>Github</h3>
+        <p>Guthub Commands</p>
+        <p>git clone git@github.com:lopeaa/prototypes.git OR git@github.com:lopeaa/Certificate-Authority.git</p>
+        <p>git add . OR git add -A</p>
+        <p>git commit </p>
+      </div>
+      
+      <!-- #################################################################### -->
+
+    	<div id="le" class="tab-pane fade">
+      	<h3>Let's Encrypt Web App</h3>
+         	<p>Web app for having Let's Encrypt signing CSR and returning a public certificate chain.</p>
+    	</div>	
+    	<!-- #################################################################### -->
+      
+      </div>
+    </div>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>&#128128; Prototypes</title>
+
+        <!-- Fonts -->
+        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
+        
+        <link href="/css/app.css" rel="stylesheet">
+	
+	   <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="css/welcome.css">
+        
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+               
+                    Prototypes
+
+                    <blockquote>Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.</blockquote>
+
+                </div>
+
+		@if (Route::has('login'))
+                <div class="">
+                    <a class='btn btn-primary btn-lg' href="{{ url('/login') }}">Login</a>
+                    <a class='btn btn-primary btn-lg' href="{{ url('https://github.com/lopeaa') }}">Github</a>
+                </div>
+               @endif
+               <br />
+               <div class="footer">
+               &#128128; Lopeaa, 2017.
+               </div>
+            </div>
+        </div>
+    </body>
+</html>
