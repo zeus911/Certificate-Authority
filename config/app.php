527c24a780c38fd644d93938c871c559205ed800
Antonio López <lopeaa@gmail.com>
2017-06-09 12:56:20 +0200

    Create app.php

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => '&#128128; Prototypes',
+    'name' => 'Prototypes',
 
     /*
     |--------------------------------------------------------------------------
