1e16d2f49ef2a7326d9991090cacd57f1b468128
Lopeaa <lopeaa@gmail.com>
2017-06-22 17:56:30 +0200

    Added layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-
+@extends('layouts.login')
 
 @section('content')
 <div class="container">
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <title>{{ config('app.name', '') }}</title>
+
+    <!-- .ico -->
+    <link rel="icon" href="{{URL::asset('favicon.ico') }}"/>
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+    <div id="app">
+
+        <nav class="navbar navbar-default navbar-static-top">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('#') }}">
+                    <!-- <div class="container"><img src="{{URL::asset('/img/logo.gif')}}" alt="LIQUABIT - Home"> -->
+                        {{ config('app.name', 'Certificate Authority') }}
+                    </a>
+                </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
+</body>
+</html>
