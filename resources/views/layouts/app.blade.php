ec29a84026f7474e8cfda867f569b5b9d9cc3b77
lopeaa@gmail.com <fe8ffc701407aa179ae18c7a60a6161ada3ab4bb>
2017-06-28 18:41:55 +0200

    Addedd Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
     <!-- Scripts -->
     <script src="/js/app.js"></script>
 </body>
-        <nav class="navbar navbar-default navbar-fixed-bottom">
+        <nav class="navbar navbar-default navbar-static-bottom">
             <div class="container">
                 <div class="navbar-header">
 
@@ -118,25 +118,14 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                        <!--
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        -->
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
+                            <li class="dropdown">
                                 <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
+                                    <li><a href="{{ url('/logout') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Logout
-                                        </a>
+                                        
 
                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
