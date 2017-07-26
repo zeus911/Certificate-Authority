8e64c38f72103d40cdc02fa5ad33356dac796cda
Lopeaa <lopeaa@gmail.com>
2017-07-26 17:11:32 +0200

    Added Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,10 +10,10 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label"></label>
+                            <label for="username" class="col-md-4 control-label" placeholder="Username">Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }} required autofocus placeholder="Username">
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus placeholder="Username">
 
                                 @if ($errors->has('username'))
                                     <span class="help-block">
