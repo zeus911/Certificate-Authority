a802c41ab89180dad4c84e298aaa026e23abde5a
Lopeaa <lopeaa@gmail.com>
2017-08-02 12:21:52 +0200

    Added Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><blockquote>Certificate Authority</blockquote></div>
+                <div class="panel-heading"><blockquote>Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning...</blockquote></div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
