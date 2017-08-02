89ae143d5e84d43b114c925c09d37a704484bbf6
Lopeaa <lopeaa@gmail.com>
2017-08-02 12:24:02 +0200

    Added Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><blockquote>Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning...</blockquote></div>
+                <div class="panel-heading">Login to Certificate Authority</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -75,9 +75,10 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                             <blockquote>
+                                Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning...
+                            </blockquote>
                                 <a class="text-default"><strong>IP Address:</strong> {{ $_SERVER['REMOTE_ADDR'] }}</a><br />
                                 <a class="text-default"><strong>DNSBL/RBL Check:</strong></a>
-                            </blockquote>    
                             </div>
                         </div>    
                     </form>
