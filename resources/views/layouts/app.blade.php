2d326970ac5962986bc6dd21d8022b83793909ab
Lopeaa <lopeaa@gmail.com>
2017-08-02 13:26:07 +0200

    Added Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,50 +91,5 @@
     <!-- Scripts -->
     <script src="/js/app.js"></script>
 </body>
-        <nav class="navbar navbar-default navbar-fixed-bottom">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand">
-                    <!-- <a class="navbar-brand" href="{{ url('/') }}"> -->
-                        {{ config('app.footer', '') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-
-                            <li>
-                                <ul>
-                                    <li class="btn btn-default navbar-btn"><a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout | Latch
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('layouts/footer')
 </html>
