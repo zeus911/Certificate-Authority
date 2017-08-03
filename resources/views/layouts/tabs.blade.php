fb530bd2307dde20dc2b0a337f18d4c8984a34a1
Lopeaa <lopeaa@gmail.com>
2017-08-03 20:42:50 +0200

    Added Layout

--- resources/views/layouts/tabs.blade.php
+++ resources/views/layouts/tabs.blade.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Prototypes') }}</title>
+
+    <!-- .ico -->
+    <link rel="icon" href="{{URL::asset('favicon.ico') }}"/>
+
+
+    <!-- Styles -->
+    <link href="/css/tabs.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+
       <h1>Responsive CSS Tabs</h1>
     <div class="tab_container">
       <input id="tab1" type="radio" name="tabs" checked>
