71c19093168718c7e864e58eb469ecd1b5970959
Lopeaa <lopeaa@gmail.com>
2017-08-03 20:39:05 +0200

    Added Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-@extends('layouts.tabs')
 
 @section('content')
 <div class="container">
@@ -7,6 +6,7 @@
     <blockquote>There are two theories to arguing with a woman. Neither works.</blockquote>
     
       <h2>Pills &amp Resources</h2>
+      @include('layouts.tabs');
     	<p></p>
   		<ul class="nav nav-pills">
     			<li class='active'><a data-toggle="pill" href="#certauth">Cetificate Authority</a></li>
