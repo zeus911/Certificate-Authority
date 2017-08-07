f37235422b7aa9c95fa494e117a442dd064d7d32
Lopeaa <lopeaa@gmail.com>
2017-08-07 19:06:10 +0200

    Added layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,10 +36,9 @@
                 </div>
                @endif
                <br />
+
                @include('layouts.footer')
-               <div class="footer">
-               <strong>&#128128; Lopeaa, 2015 - {{ date('Y') }}.</strong>
-               </div>
+
             </div>
         </div>
     </body>
