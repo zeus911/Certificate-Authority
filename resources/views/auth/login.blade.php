3313e216c7404a04a473afe40957fd8deb798dd9
Lopeaa <lopeaa@gmail.com>
2017-08-21 18:29:32 +0200

    Added Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login to Certificate Authority</div>
+                <div class="panel-heading">Login to Certificate Authority (Prototype)</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -75,7 +75,7 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <a class="text-default"><strong>IP Address:</strong> {{ $_SERVER['REMOTE_ADDR'] }}</a><br />
-                                <a class="text-default"><strong>DNSBL/RBL Check:</strong></a>
+                                <a class="text-default"><strong>DNSBL/RBL Check: PASS</strong></a>
                             </div>
                         </div> 
                             <blockquote>
