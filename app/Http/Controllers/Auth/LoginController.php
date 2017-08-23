da7cabf17d4198b0b4bc4d00a02eb0360884c132
Lopeaa <lopeaa@gmail.com>
2017-08-23 12:55:43 +0200

    Add Latch

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+/*class LoginController extends Controller
+{
+
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/home';
+    
+    // Create a new controller instance.
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+}*/
+
+class LoginController extends BaseController {
+
+    use AuthenticatesUsers;
+    
+    public function login()
+    {
+        // Datos del formulario enviados por el usuario
+        //$input = Input::all();
+        if(isset($_POST['username']) &&  
+             isset($_POST['password']) &&  
+             isset($_POST['otc'])
+
+             $input = $_POST['cn'] + $_POST['password'] + $_POST['otc'];
+
+        dd($input);
+
+        // Reglas de validacion
+        $rules = array(
+            'username'       => 'required|username',
+            'password' => 'required'
+        );
+        // validamos los datos del formulario de login
+        $validator = Validator::make($input, $rules);
+        if ($validator->passes())
+        {
+            // Credenciales para el inicio de sesion del usuario
+            $credentials = array('username' => $input['username'], 'password' => $input['password']);
+            // Establece si tenemos acceso para acceder a nuestra cuenta de usuario
+            $locked = true;
+            // Comprobamos si el usuario es valido pero sin loguearlo
+            if (Auth::validate($credentials))
+            {
+                // Obtenemos el identificador del usuario de Latch de nuestra base de datos (con sql)
+                $user          = User::where('username', '=', $input['username'])->first();
+                $accountId = $user->latch_account_id;
+                // Comprueba si Latch nos da acceso
+                if (Latch::unlocked($accountId))
+                {
+                    $locked = false;
+                }
+            }
+            // Si la cuenta del usuario no esta bloqueada
+            if ( ! $locked)
+            {
+                // Autentica al usuario
+                if (Auth::attempt($credentials))
+                {
+                    //
+                }
+            }
+        }
+    }
+}
