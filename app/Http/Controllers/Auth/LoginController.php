f53a2b77458f861c8f008c6a09ba45815e59b741
Lopeaa <lopeaa@gmail.com>
2017-08-23 09:59:17 +0200

    Add Latch

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,7 +16,7 @@ class LoginController extends BaseController {
         $input = Input::all();
         // Reglas de validacion
         $rules = array(
-            'email'       => 'required|email',
+            'username'       => 'required|username',
             'password' => 'required'
         );
         // validamos los datos del formulario de login
@@ -24,14 +24,14 @@ class LoginController extends BaseController {
         if ($validator->passes())
         {
             // Credenciales para el inicio de sesion del usuario
-            $credentials = array('email' => $input['email'], 'password' => $input['password']);
+            $credentials = array('username' => $input['username'], 'password' => $input['password']);
             // Establece si tenemos acceso para acceder a nuestra cuenta de usuario
             $locked = true;
             // Comprobamos si el usuario es valido pero sin loguearlo
             if (Auth::validate($credentials))
             {
                 // Obtenemos el identificador del usuario de Latch de nuestra base de datos (con sql)
-                $user          = User::where('email', '=', $input['email'])->first();
+                $user          = User::where('username', '=', $input['username'])->first();
                 $accountId = $user->latch_account_id;
                 // Comprueba si Latch nos da acceso
                 if (Latch::unlocked($accountId))
