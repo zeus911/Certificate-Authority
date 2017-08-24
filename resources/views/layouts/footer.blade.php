27a8fe8124bc6af328693505150f220650c09903
Lopeaa <lopeaa@gmail.com>
2017-08-24 10:21:22 +0200

    Latch

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,5 @@
 <div class="footer">
-	<strong><a href="auth/latch">Protect your account with Latch</a></strong>
     <strong>&#128128; Lopeaa, 2015 - {{ date('Y') }}</strong>
+	<strong><a href="auth/latch"><i class="fa fa-shield" aria-hidden="true"></i> Latch</a></strong>
+
 </div>
