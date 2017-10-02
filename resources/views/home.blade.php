8093ed1d7dd371e2b4e86cad87fcec9670970214
lopeaa <lopeaa@gmail.com>
2017-10-02 19:02:38 +0200

    Change template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,43 @@
     
       <h2>Pills &amp Resources</h2>
     	<p></p> 
-      {{ Form::label('certificate type: ', 'Certificate Type: ', ['class' => '']) }}
-      {{ Form::select('certificate_type', ['TLS Web Server' => 'TLS Web Server', 'Client Authentication' => 'Client Authentication', 'Code Signing' => 'Code Signing'], null, ['placeholder' => 'Select certificate type', 'class' => 'form-control' ]) }}
-        @if($errors->has('certificate_type'))
-        {{ $errors->first('certificate_type') }} 
-      @endif
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Quick Example</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox"> Check me out
+                  </label>
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
       <br />
 
       <ul class="nav nav-pills"> 
