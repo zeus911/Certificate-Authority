4b7faa793a72da0365293d4f82f6ded825385745
Lopeaa <lopeaa@gmail.com>
2017-10-03 12:59:30 +0200

    Template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,6 @@
 @section('content')
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
 
     <title>
       
@@ -39,74 +32,6 @@
 
 <body class="bjy">
   <div class="bkk" id="app-growl"></div>
-
-<nav class="ck mo zq mt qh app-navbar">
-
-  <a class="e" href="../index.html">
-    <img src="/img/brand-white.png" alt="brand">
-  </a>
-
-  <button
-    class="mk blk se"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarResponsive"
-    aria-controls="navbarResponsive"
-    aria-expanded="false"
-    aria-label="Toggle navigation">
-    <span class="ml"></span>
-  </button>
-
-  <div class="collapse f" id="navbarResponsive">
-    <ul class="navbar-nav adn">
-      <li class="md">
-        <a class="mb" href="../index.html">Home <span class="zt">(current)</span></a>
-      </li>
-      <li class="md">
-        <a class="mb" href="../profile/index.html">Profile</a>
-      </li>
-      <li class="md">
-        <a class="mb" data-toggle="modal" href="#msgModal">Messages</a>
-      </li>
-      <li class="md active">
-        <a class="mb" href="../docs/index.html">Docs</a>
-      </li>
-
-      <li class="md se">
-        <a class="mb" href="../notifications/index.html">Notifications</a>
-      </li>
-      <li class="md se">
-        <a class="mb" data-action="growl">Growl</a>
-      </li>
-      <li class="md se">
-        <a class="mb" href="../login/index.html">Logout</a>
-      </li>
-
-    </ul>
-
-    <form class="kl zc ro sk">
-      <input class="form-control" type="text" data-action="grow" placeholder="Search">
-    </form>
-
-    <ul id="#js-popoverContent" class="nav navbar-nav zc aah ro sk">
-      <li class="md">
-        <a class="g mb" href="../notifications/index.html">
-          <span class="h avv"></span>
-        </a>
-      </li>
-      <li class="md aax">
-        <button class="cg bll blm bkf" data-toggle="popover">
-          <img class="rl" src="../assets/img/avatar-dhg.png">
-        </button>
-      </li>
-    </ul>
-
-    <ul class="nav navbar-nav ro" id="js-popoverContent">
-      <li class="md"><a class="mb" href="#" data-action="growl">Growl</a></li>
-      <li class="md"><a class="mb" href="../login/index.html">Logout</a></li>
-    </ul>
-  </div>
-</nav>
 
   <div class="cd fade" id="msgModal" tabindex="-1" role="dialog" aria-labelledby="bln" aria-hidden="true">
   <div class="modal-dialog">
