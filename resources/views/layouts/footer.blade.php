ed554988a779c8c6772f564c08bf2e5ddb885213
lopeaa <lopeaa@gmail.com>
2017-10-03 16:11:07 +0200

    Test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <strong><i class="fa fa-flask" aria-hidden="true"></i> Lopeaa: 2015 - {{ date('F, Y') }}</strong>
+    <strong><i class="fa fa-flask" aria-hidden="true" style="color:red;></i> Lopeaa: 2015 - {{ date('F, Y') }}</strong>
 </div>
