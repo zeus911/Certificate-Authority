b92889c0b608f5aa3cf39385c22dd370cc25ef0c
Lopeaa <lopeaa@gmail.com>
2017-10-18 16:06:11 +0200

    Layouts lift

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <strong><i class="fa fa-flask" style="color:red;" aria-hidden="true"></i> Lopeaa: 2015 - {{ date('F, Y') }}</strong>
+    <strong><i class="fa fa-flask" style="color:orange;" aria-hidden="true"></i> Lopeaa: 2015 - {{ date('F, Y') }}</strong>
 </div>
