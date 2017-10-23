6d12b5b145e2e9c61f867efda1f888dd2c291b6d
lopeaa <lopeaa@gmail.com>
2017-10-23 18:45:05 +0200

    New Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><strong><i class="fa fa-lock" aria-hidden="true"></i></strong> Login to Certificate Authority (Procyon)</div>
+                <div class="panel-heading"><strong><i class="fa fa-lock" aria-hidden="true"></i></strong> Login to Certificate Authority (Demo)</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -64,7 +64,7 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-sign-in" aria-hidden="true"></i>
-                                    Login
+                                    Login to CA Demo
                                 </button>
 
                                 <!-- <a class="btn btn-link" href="{{ url('/password/reset') }}">
