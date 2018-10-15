b8f7b878b27099592ad7e3070c8286a4812a0887
Lopeaa <lopeaa@gmail.com>
2018-10-15 17:44:30 +0200

    New Cert.php

--- app/Cert.php
+++ app/Cert.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cert extends Model
 {
-	protected $fillable = ['cn', 'certificate_type', 'digest_alg', 'serial', 'csrprint','certprint','keyprint', 'p12'];
+	protected $fillable = ['cn', 'certificate_type', 'digest_alg', 'serial', 'csrprint','certprint','keyprint', 'p12', 'status'];
 	
 }
